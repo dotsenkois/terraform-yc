2d6cabda10ab57a98d6aad32ea4d95b70c47a8d7
dotsenkois <dotsenkois@yandex.ru>
2021-12-29 13:59:16 +0700

    07-terraform-03-basic

--- main.tf
+++ main.tf
@@ -1,29 +1,70 @@
 
+#Опсиание backend находится в файле backend.tf
+#Данный файл содержит ключи сервиснго акаунта и добавлен в .gitignore(не удалось записать в переменные окружения)
+
+/*
+terraform {
+    backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "dotsenkois-netology"
+    region     = "ru-central1-c"
+    key        = "main-conf/test.tfstate"
+    access_key = 
+    secret_key = 
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+*/
+
+
 provider "yandex" {
-  #token     = "${var.YC_TOKEN}"
-  #cloud_id  = "${var.YC_CLOUD_ID}"
-  #folder_id = "${var.YC_FOLDER_ID}"
-  zone      = "ru-central1-c"
+  zone = "ru-central1-c"
+  #Ключи сохранены в переменых среды:
+  #YC_TOKEN, YC_CLOUD_ID, YC_FOLDER_ID
 }
 
-resource "yandex_compute_instance" "vm-test-netology" {
-  name = "test-netology"
+locals {
+  web_instance_type_map = {
+    stage = "standard-v1"
+    prod  = "standard-v2"
+  }
+  web_instance_count_map = {
+    stage = 1
+    prod  = 2
+  }
+  web_instance_name_map = {
+    stage = "test-netology-stage"
+    prod  = "test-netology-prod"
+  }
+  instances = {
+    "standard-v1" = "fd8f30hur3255mjfi3hq"
+    "standard-v2" = "fd8f30hur3255mjfi3hq"
+  }
 
-  platform_id = "standard-v2"
-  hostname = "helloworld_01"
+}
+
+
+resource "yandex_compute_instance" "count" {
+
+  name        = format("count-%03d", count.index + 1)
+  platform_id = local.web_instance_type_map[terraform.workspace]
+  hostname    = format("count-%03d", count.index + 1)
   description = "test vm for netology"
+  count       = local.web_instance_count_map[terraform.workspace]
 
 
   resources {
-    cores  = "2"
-    memory = "1"
+    cores         = "2"
+    memory        = "1"
     core_fraction = "20"
   }
   boot_disk {
     initialize_params {
       image_id = "fd8f30hur3255mjfi3hq"
-      type = "network-hdd"
-      size = "5"
+      type     = "network-hdd"
+      size     = "5"
     }
   }
   network_interface {
@@ -34,6 +75,38 @@ resource "yandex_compute_instance" "vm-test-netology" {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = "true"
   }
+}
+
+
+resource "yandex_compute_instance" "foreach" {
+  for_each    = local.instances
+  platform_id = each.key
+
+  resources {
+    cores         = "2"
+    memory        = "1"
+    core_fraction = "20"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = each.value
+      type     = "network-hdd"
+      size     = "5"
+    }
+  }
+  network_interface {
+    subnet_id = "b0cgoir8ev0gjdgfn8ko"
+    nat       = "true"
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  scheduling_policy {
+    preemptible = "true"
+  }
+
 }
